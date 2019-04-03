3b13e6a98398c9e757feb49946a098806cfca9fc
Lauren Cochran <laurenann.cochran@gmail.com>
2019-04-02 20:45:24 -0500

    redid the background color so it's in a struct

---
+++ Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Lauren on 2019-04-02.
+//  Copyright © 2019 Lauren Cochran. All rights reserved.
+//
+
+import UIKit
+
+struct Colors{
+    let name: String
+    let uiColor: UIColor 
+}
--- Colors/ColorsViewController.swift
+++ Colors/ColorsViewController.swift
@@ -9,8 +9,14 @@
 import UIKit
 
 class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    
+    var colors = [Colors(name: "red", uiColor: UIColor.red),
+                  Colors(name: "orange", uiColor: UIColor.orange),
+                  Colors(name: "yellow", uiColor: UIColor.yellow),
+                  Colors(name: "green", uiColor: UIColor.green),
+                  Colors(name: "blue", uiColor: UIColor.blue),
+                  Colors(name: "purple", uiColor: UIColor.purple),
+                  Colors(name: "brown", uiColor: UIColor.brown)]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,38 +29,18 @@ class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colors.count
-    }
-    func backgroundColor(index: Int) -> UIColor {
-        switch index{
-        case 0:
-            return UIColor.red
-        case 1:
-            return UIColor.orange
-        case 2:
-            return UIColor.yellow
-        case 3:
-            return UIColor.green
-        case 4:
-            return UIColor.blue
-        case 5:
-            return UIColor.purple
-        case 6:
-            return UIColor.brown
-        default:
-            break
-        }
-        return UIColor.white
-    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
         
-        cell.textLabel?.text = colors[indexPath.row]
-        cell.backgroundColor = backgroundColor(index: indexPath.row)
+        cell.textLabel?.text = colors[indexPath.row].name
+        cell.backgroundColor = colors[indexPath.row].uiColor
+        
         return cell
     }
     
 
     
 
+}
 }
